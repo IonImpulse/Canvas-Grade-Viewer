246a613633f9f47557d5e0770cdc0cbfbbf857c3
Ethan <edv121@outlook.com>
2019-10-23 09:32:22 -0700

    more

--- canvasgradeviewer.client.js
+++ canvasgradeviewer.client.js
@@ -1,10 +1,9 @@
+document.body.style.border = "5px solid red";
 console.log("Canvas Grade Viewer loaded. Ha ha teachers.");
+
 var assignments = document.getElementsByClassName("student_assignment assignment_graded editable");
-console.log(assignments);
-browser.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    if (request.command == "requestGrade") {
-        var assignments = document.getElementsByClassName("student_assignment assignment_graded editable");
-        console.log(assignments);
-        }
-    }
-});
+var i;
+for (i = 0; i < x.length; i++) {
+  console.log("Red");
+  assignments[i].style.backgroundColor = "red";
+}
--- canvasgradeviewer.dashboard.js
+++
@@ -1,6 +0,0 @@
-browser.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    browser.tabs.sendMessage(tabs[0].id, { command: "requestGrade" }, (response) => {
-        this._gradesList = JSON.parse(response);
-        console.log(this._gradesList);
-    });
-});
