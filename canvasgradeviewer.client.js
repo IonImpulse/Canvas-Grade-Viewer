aa2eb1224482ff1b5446229920eec4679ec918af
Ethan <edv121@outlook.com>
2019-10-23 13:40:35 -0700

    started on weights

--- canvasgradeviewer.client.js
+++ canvasgradeviewer.client.js
@@ -7,11 +7,17 @@ document.addEventListener('DOMContentLoaded', function() {
       }
       return stringNum
     }
+
+    function isolateWeight(weightString) {
+      
+    }
     var assignmentsList = document.getElementsByClassName("student_assignment editable");
     var grades = [];
     var gradeMaxes = [];
     var gradeTypes = [];
-    console.log(assignmentsList[0]);
+
+    console.log(assignmentsList[0].innerHTML.split("\n"));
+
     for (var i = 0; i < assignmentsList.length; i++) {
         var tempAssignment = assignmentsList[i].innerHTML.split("\n");
         var lengthInner = tempAssignment.length;
@@ -20,7 +26,10 @@ document.addEventListener('DOMContentLoaded', function() {
 
         gradeMaxes.push(checkZeroConvert(tempAssignment[lengthInner-31].replace(/\s/g,'')));
 
+        gradeTypes.push(tempAssignment[4]);
     }
+
     console.log(grades);
     console.log(gradeMaxes);
+    console.log(gradeTypes);
 }, false);
