5dddd61398307d2391f2c1fd7cf13d7464f8e1cb
Ethan <edv121@outlook.com>
2019-10-23 14:58:29 -0700

    finished weight detection

--- canvasgradeviewer.client.js
+++ canvasgradeviewer.client.js
@@ -1,15 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
     function checkZeroConvert(stringNum) {
-      if (stringNum == "") {
-          stringNum = 0.0;
-      } else {
-          stringNum = parseFloat(stringNum);
-      }
-      return stringNum
+        if (stringNum == "") {
+            stringNum = 0.0;
+        } else {
+            stringNum = parseFloat(stringNum);
+        }
+        return stringNum;
     }
 
     function isolateWeight(weightString) {
-      
+        weightString = weightString.slice(weightString.indexOf(">") + 1, weightString.indexOf("<", weightString.indexOf(">")));
+        return weightString;
     }
     var assignmentsList = document.getElementsByClassName("student_assignment editable");
     var grades = [];
@@ -26,10 +27,39 @@ document.addEventListener('DOMContentLoaded', function() {
 
         gradeMaxes.push(checkZeroConvert(tempAssignment[lengthInner-31].replace(/\s/g,'')));
 
-        gradeTypes.push(tempAssignment[4]);
+        gradeTypes.push(isolateWeight(tempAssignment[4]));
     }
 
     console.log(grades);
     console.log(gradeMaxes);
     console.log(gradeTypes);
+
+    var weightsListRaw = document.getElementById("assignments-not-weighted").innerHTML.split("\n");
+
+    if (isolateWeight(weightsListRaw[3]) == "Assignments are weighted by group:") {
+        var weightsExist = true;
+
+        var weightNames = []
+        var weightValues = []
+        var iterations = weightsListRaw.length - 15
+
+        console.log(weightsListRaw);
+
+        for (i = 14; i < iterations; i = i + 4) {
+            var tempWeightName = weightsListRaw[i];
+            var tempWeightValue = weightsListRaw[i + 1];
+
+            weightNames.push(isolateWeight(tempWeightName));
+            weightValues.push(parseFloat(isolateWeight(tempWeightValue)));
+        }
+
+        console.log(weightNames);
+        console.log(weightValues);
+    } else {
+        var weightsExist = false;
+    }
+
+
+
+
 }, false);
