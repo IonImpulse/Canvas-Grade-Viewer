0b30be43d44396a538489f04b77c8409d92ce876
Ethan <edv121@outlook.com>
2021-12-02 08:30:17 -0800

    update to manifest v3

--- canvasgradeviewer.background.js
+++
@@ -1,5 +0,0 @@
-// Set survey link on uninstallation:
-
-if(chrome.runtime.setUninstallURL) {
-    chrome.runtime.setUninstallURL('https://forms.gle/Ypc1WXU14aoqNwHF6');
-}
--- canvasgradeviewer.client.js
+++ canvasgradeviewer.client.js
@@ -498,3 +498,8 @@ document.addEventListener('keydown', function(e) {
         WaitForGrade();
     }
 }, false);
+
+// Set survey link on uninstallation:
+if(chrome.runtime.setUninstallURL) {
+    chrome.runtime.setUninstallURL('https://forms.gle/Ypc1WXU14aoqNwHF6');
+}
